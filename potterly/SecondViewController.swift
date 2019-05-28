73d0fa8afe72fc7472699af4caa0806a839d2771
lexspirtes <aspirtes@wesleyan.edu>
2019-05-28 12:59:13 -0400

    Initial Commit

---
+++ potterly/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  potterly
+//
+//  Created by Lex Spirtes on 5/28/19.
+//  Copyright © 2019 Lex Spirtes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
