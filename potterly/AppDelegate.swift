2f8f370860403d6b32c583c8efdd45a987a6efbf
lexspirtes <aspirtes@wesleyan.edu>
2019-05-31 12:16:57 -0400

    added tabBarController

--- potterly/AppDelegate.swift
+++ potterly/AppDelegate.swift
@@ -13,14 +13,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    var navigationController: UINavigationController?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame:UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = NavViewController()
+        window?.rootViewController = SecondViewController()
         return true
     }
+    
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Override point for customization after application launch.
+//
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//
+//        if let window = window {
+//            let mainVC = UIViewController()
+//            navigationController = UINavigationController(rootViewController: mainVC)
+//            window.rootViewController = navigationController
+//            window.makeKeyAndVisible()
+//        }
+//
+//        return true
+//    }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- potterly/StageViewController.swift
+++ potterly/StageViewController.swift
@@ -14,19 +14,27 @@ import PinLayout
 
 class NavBar: UIView {
     fileprivate let rootFlexContainer = UIView()
-    fileprivate let navFlexContainer = UIView()
     fileprivate let topBanner = UIView()
+    fileprivate let contentView = UIScrollView()
+    fileprivate let navView = UIView()
     fileprivate let view2 = UIView()
     let color = colors()
     init() {
         super.init(frame: .zero)
-        backgroundColor = .lightGray
+        backgroundColor = .white
         
         //banner
         topBanner.backgroundColor = .gray
         let potterly = UILabel()
         potterly.formatLabel(text: "potterly", spacing: 2, font: "BodoniSvtyTwoITCTT-Book", fontSize: 30)
         
+        //creating topBanner as flex container
+        topBanner.flex.addItem().height(96).direction(.column).backgroundColor(color.darkBlue).define { (flex) in
+            flex.justifyContent(.end).alignItems(.center).paddingBottom(10).addItem(potterly)
+        }
+        
+        //trying to change thing
+        rootFlexContainer.backgroundColor = .lightGray
         
         //done button and label
         let doneLabel = UILabel()
@@ -58,49 +66,49 @@ class NavBar: UIView {
         let addImage = UIImage(named: "add")
         let addButton = UIButton(type: UIButton.ButtonType.custom)
         addButton.setImage(addImage, for: .normal)
-        
-    rootFlexContainer.flex.direction(.column).paddingBottom(10).justifyContent(.spaceEvenly).define { (flex) in
-        
-            flex.addItem().height(96).direction(.column).backgroundColor(color.darkBlue).define { (flex) in
-                flex.justifyContent(.end).alignItems(.center).paddingBottom(10).addItem(potterly)
+    
+    
+        navView.flex.direction(.column).direction(.column).addItem().backgroundColor(.white).marginBottom(10).define { (flex) in
+            //adding purple line to bottom
+            flex.addItem().height(2).backgroundColor(color.lightPurple).marginBottom(10)
+            flex.addItem().backgroundColor(.white).direction(.row).alignItems(.center).justifyContent(.spaceAround).define { (flex) in
+                //creating home button
+                flex.addItem().direction(.column).justifyContent(.center).define { (flex) in
+                    flex.addItem(homeButton)
+                    flex.marginBottom(5).addItem(homeLabel)
+                    
+                }
+                //creating notes button
+                flex.addItem().direction(.column).justifyContent(.center).define { (flex) in
+                    flex.addItem(notesButton)
+                    flex.marginBottom(5).addItem(notesLabel)
+                    
+                }
+                //creating add button
+                flex.addItem().direction(.column).justifyContent(.center).define { (flex) in
+                    flex.addItem(addButton)
+                    flex.marginBottom(5).addItem(addLabel)
+                    
+                }
+                //creating done button
+                flex.addItem().direction(.column).justifyContent(.center).define { (flex) in
+                    flex.addItem(doneButton)
+                    flex.marginBottom(5).addItem(doneLabel)
+                    
+                }
             }
-            flex.addItem().backgroundColor(.lightGray)
-            //line and bar one flex item
-            flex.addItem().backgroundColor(.white).direction(.column).marginBottom(10).define { (flex) in
-                //adding purple line to bottom
-                flex.addItem().height(2).backgroundColor(color.lightPurple).marginBottom(10)
-                flex.addItem().backgroundColor(.white).direction(.row).alignItems(.center).justifyContent(.spaceAround).define { (flex) in
-                    //creating home button
-                    flex.addItem().direction(.column).justifyContent(.center).define { (flex) in
-                        flex.addItem(homeButton)
-                        flex.marginBottom(5).addItem(homeLabel)
-                        
-                    }
-                    //creating notes button
-                    flex.addItem().direction(.column).justifyContent(.center).define { (flex) in
-                        flex.addItem(notesButton)
-                        flex.marginBottom(5).addItem(notesLabel)
-                        
-                    }
-                    //creating add button
-                    flex.addItem().direction(.column).justifyContent(.center).define { (flex) in
-                        flex.addItem(addButton)
-                        flex.marginBottom(5).addItem(addLabel)
-                        
-                    }
-                    //creating done button
-                    flex.addItem().direction(.column).justifyContent(.center).define { (flex) in
-                        flex.addItem(doneButton)
-                        flex.marginBottom(5).addItem(doneLabel)
-                        
-                    }
-                    }
         }
-        
-        
-    }
-        
+
+//    rootFlexContainer.flex.direction(.column).paddingBottom(10).define { (flex) in
+//
+//      //  flex.addItem(topBanner)
+//        flex.addItem().backgroundColor(.lightGray)
+//      //  flex.addItem(navView)
+//        }
+        rootFlexContainer.flex.height(300)
         addSubview(rootFlexContainer)
+        addSubview(topBanner)
+        addSubview(navView)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -113,12 +121,17 @@ class NavBar: UIView {
         // Layout the flexbox container using PinLayout
         // NOTE: Could be also layouted by setting directly rootFlexContainer.frame
             rootFlexContainer.pin.top().horizontally()
+            topBanner.pin.top().horizontally()
+            navView.pin.bottom(pin.safeArea).horizontally()
         // Then let the flexbox container layout itself
             rootFlexContainer.flex.layout(mode: .adjustHeight)
+        navView.flex.layout(mode: .adjustHeight)
+        topBanner.flex.layout(mode: .adjustHeight)
     }
 }
 
 class NavViewController: UIViewController {
+    
     //make status bar white
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -128,11 +141,18 @@ class NavViewController: UIViewController {
         return .lightContent
     }
     
-    fileprivate var mainView: IntroView {
-        return self.view as! IntroView
+    fileprivate var mainView: NavBar {
+        return self.view as! NavBar
     }
     
+  //  load view function
     override func loadView() {
         view = NavBar()
     }
+    
+    //view did load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
 }
